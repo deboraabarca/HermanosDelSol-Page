88ca9985d0496166fc7a85ab24aee9e75946a4a7
debora abarca <abarcazavalad@gmail.com>
2024-09-17 20:18:06 -0500

    final page(falta redes sociales)

--- js/animation.js
+++ js/animation.js
@@ -4,8 +4,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver(entries => {
     if (entries[0].isIntersecting) {
       weSection.classList.add('animate');
-      observer.disconnect(); // Deja de observar tras activarse
+      observer.disconnect(); 
     }
   });
   observer.observe(weSection);
+});
+// scroll 
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll("a[href^='#']");
+
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth"
+      });
+    });
+  });
 });
